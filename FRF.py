fe135249165efaa8c334b84ced011fe322f3724a
moosa <ahmemo@ohsu.edu>
2018-05-11 14:15:04 -0700

    reorganised and added documentations for functions

--- FRF.py
+++ FRF.py
@@ -22,20 +22,52 @@ class CallForest(object):
 
     def train_method(self, X_train, y_train, X_test, y_test):
         if self.isclassifier:
-            return self.called_method(learning_data=X_train, learning_groups=y_train, testing_data=X_test,
+            return self.called_method(training_data=X_train, training_groups=y_train, testing_data=X_test,
                                       testing_groups=y_test, n_trees=self.n_trees, n_predictors=self.n_predictors,
                                       oob_score=self.oob_score, feature_importance=self.feature_importance,
                                       class_weight=self.class_weight)
         else:
-            return self.called_method(learning_data=X_train, learning_groups=y_train, testing_data=X_test,
+            return self.called_method(training_data=X_train, training_groups=y_train, testing_data=X_test,
                                       testing_groups=y_test, n_trees=self.n_trees, n_predictors=self.n_predictors,
                                       oob_score=self.oob_score, feature_importance=self.feature_importance)
 
 
-def get_feature_importance(model, learning_data):
+def build_kfolds(df, data_cols, target_cols, forest_params, n_splits=10, n_repeats=3):
+    """
+    This takes a pandas df with headers and two lists denoting the data columns and the target columns.
+    It also inherits user specified parameters for the rf model.
+
+    This sets up the RepeatedStratifiedKFold func, and makes a test and train split based on the parameters provided.
+    For each fold it runs the model and creates a summary of the test indicies used and the rf models outputs.
+
+    Returns a full summary of all folds and their results
+    """
+    X = df[data_cols]
+    y = df[target_cols]
+    rskf = RepeatedStratifiedKFold(n_splits, n_repeats, random_state=int(timeit.default_timer()))
+    summary = {}
+    for train_index, test_index in rskf.split(X, y):
+        # print("TRAIN:", train_index, "TEST:", test_index)
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        print(forest_params.train_method(X_train, y_train, X_test, y_test))
+        # summary[test_index] = forest_stats
+        # TODO: make the numpy array hashable
+    return summary
+
+
+def get_feature_importance(model, training_data):
+    """
+    This takes a model and the training data used to train the model.
+    The training data should be a pandas df with the feature names in the headers.
+
+    Returns an ordered list of tuples. Ordered from most important feature to least.
+    The tuple has the feature name and a decimal number denoting importance as a ratio.
+    """
     features_dict = {}
     for i in range(len(model.feature_importances_)):
-        features_dict[list(learning_data)[i]] = model.feature_importances_[i]
+        features_dict[list(training_data)[i]] = model.feature_importances_[i]
     return sorted(features_dict.items(), key=lambda x: x[1], reverse=True)
 
 
@@ -56,17 +88,17 @@ def proximity_matrix(model, x, normalize=True):
     return prox_mat
 
 
-def test_class_tree_bags(learning_data, learning_groups, testing_data, testing_groups, n_trees=10, n_predictors="auto",
+def test_class_tree_bags(training_data, training_groups, testing_data, testing_groups, n_trees=10, n_predictors="auto",
                          oob_score=False, feature_importance=False, class_weight=None):
     """
-    This takes at minimum 4 inputs, your learning group and data as well as testing group and data.
+    This takes at minimum 4 inputs, your training group and data as well as testing group and data.
 
     Read RandomForestClassifier documentation for more info on n_trees(n_estimators), n_predictors(max_features),
     oob_score, and class_weight.
     If a dictionary of feature importance is required set feature_importance to True
     
     This function will build the Random Forest based on parameters provided.
-    It will fit the RF on your learning set.
+    It will fit the RF on your training set.
     
     This function returns: 
     1) Predicted classes which are made using your testing data
@@ -81,7 +113,7 @@ def test_class_tree_bags(learning_data, learning_groups, testing_data, testing_g
     tree_bag = RandomForestClassifier(n_estimators=n_trees, max_features=n_predictors, oob_score=oob_score, 
                                       class_weight=class_weight)
 
-    tree_bag.fit(learning_data, learning_groups)
+    tree_bag.fit(training_data, training_groups)
     predicted_classes = tree_bag.predict(testing_data)
     overall_accuracy = accuracy_score(testing_groups, predicted_classes)
     predicted_scores = tree_bag.predict_log_proba(testing_data)
@@ -95,15 +127,15 @@ def test_class_tree_bags(learning_data, learning_groups, testing_data, testing_g
         return_list.append(oob_pred)
 
     if feature_importance:
-        return_list.append(get_feature_importance(tree_bag, learning_data))
+        return_list.append(get_feature_importance(tree_bag, training_data))
 
     return predicted_classes, predicted_scores, overall_accuracy, (*return_list)
 
 
-def test_regress_tree_bags(learning_data, learning_groups, testing_data, testing_groups, n_trees=10,
+def test_regress_tree_bags(training_data, training_groups, testing_data, testing_groups, n_trees=10,
                            n_predictors="auto", oob_score=False, feature_importance=False):
     """
-    This takes at minimum 4 inputs, your learning group and data as well as testing group and data.
+    This takes at minimum 4 inputs, your training group and data as well as testing group and data.
 
     rf_type can be chosen to be either classification or regression
     Read RandomForestRegressor documentation for more info on n_trees(n_estimators), n_predictors(max_features),
@@ -111,13 +143,13 @@ def test_regress_tree_bags(learning_data, learning_groups, testing_data, testing
     If a dictionary of feature importance is required set feature_importance to True
 
     This function will build the appropriate Random Forest based on parameters provided.
-    It will fit the RF on your learning set.
+    It will fit the RF on your training set.
 
     This function returns: 
     1) Predicted classes which are made using your testing data
-    2) Overall accuracy of the RF in predicting your testing groups
-    3) Predicted scores which are computed using the testing data
-    4) Group accuracies for every group in your testing group
+    2) Mean absolute error of the model
+    3) R-squared score of the model
+    4) Individual differences for all subjects in the predicted class versus the test groups
 
     Optionally, the function returns:
     5) Score of the training data set obtained using the out-of-bag estimate
@@ -125,7 +157,7 @@ def test_regress_tree_bags(learning_data, learning_groups, testing_data, testing
     """
     tree_bag = RandomForestRegressor(n_estimators=n_trees, max_features=n_predictors, oob_score=oob_score)
 
-    tree_bag.fit(learning_data, learning_groups)
+    tree_bag.fit(training_data, training_groups)
     predicted_classes = tree_bag.predict(testing_data)
     mae = mean_absolute_error(testing_groups, predicted_classes)
     r2 = r2_score(testing_groups, predicted_classes)
@@ -138,41 +170,17 @@ def test_regress_tree_bags(learning_data, learning_groups, testing_data, testing
         return_list.append(oob_pred)
 
     if feature_importance:
-        features_dict = {}
-        for i in range(len(tree_bag.feature_importances_)):
-            features_dict[learning_data[i]] = tree_bag.feature_importances_[i]
-            # check that it is getting the correct headers from learning set
-        return_list.append(features_dict)
+        return_list.append(get_feature_importance(tree_bag, training_data))
 
-    # if return_list:
     return predicted_classes, mae, r2, individual_diff, (*return_list)
-    # else:
-    #     return predicted_classes, mae, r2, individual_diff
-
-
-def build_kfolds(df, data_cols, target_cols, forest_params, n_splits=10, n_repeats=3):
-
-    X = df[data_cols]
-    y = df[target_cols]
-    rskf = RepeatedStratifiedKFold(n_splits, n_repeats, random_state=int(timeit.default_timer()))
-    summary = {}
-    for train_index, test_index in rskf.split(X, y):
-        # print("TRAIN:", train_index, "TEST:", test_index)
-        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-
-        print(forest_params.train_method(X_train, y_train, X_test, y_test))
-        # summary[test_index] = forest_stats
-        # TODO: make the numpy array hashable
-    return summary
 
 
 def interface(df, data_cols, target_cols, rf_type='classifier', n_trees=10, n_predictors="auto", oob_score=False,
-              feature_importance=False, class_weight=None):
+              feature_importance=False, class_weight=None, n_kfold_splits=10, n_kfold_repeats=3):
 
     forest_params = CallForest(rf_type=rf_type, n_trees=n_trees, n_predictors=n_predictors, oob_score=oob_score,
                                class_weight=class_weight, feature_importance=feature_importance)
-    out = build_kfolds(df, data_cols, target_cols, forest_params)
+    out = build_kfolds(df, data_cols, target_cols, forest_params, n_splits=n_kfold_splits, n_repeats=n_kfold_repeats)
     print(out)
 
 
